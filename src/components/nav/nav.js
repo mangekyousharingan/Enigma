db9f4202a68b99e092085b497f072bd1a0fca86b
Sebastian Wdowiarz <swdowiarz@groupon.com>
2019-07-27 13:49:29 +0200

    Add logo and change style

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -1,15 +1,23 @@
 import React from 'react';
+import locked from './locked.png'
 
 const NavBar = () => {
   return (
-    <nav className="pa3 pa4-ns">
-      <a className="link dim light-red b f1 f-headline-ns tc db mb3 mb4-ns" href="#" title="Home">Enigma</a>
-      <div className="tc pb3">
-        <a className="link dim gray f6 f5-ns dib mr3" href="#" title="Home">Encoder</a>
-        <a className="link dim gray f6 f5-ns dib mr3" href="#" title="Store">Decoder</a>
-        <a className="link dim gray f6 f5-ns dib mr3" href="#" title="About">About</a>
+    <div className="di">
+      <div className="fl w-third">
+        <img src={locked} className="grow pa3 pa4-ns" alt="avatar"/>
       </div>
-    </nav>
+      <div className="fl w-third">
+        <nav className="pa3 pa4-ns">
+          <a className="link dim light-red b f1 f-headline-ns tc db mb3 mb4-ns" href="#" title="Home">Enigma</a>
+          <div className="tc pb3">
+            <a className="link dim gray f6 f5-ns dib mr3" href="#" title="Home">Encoder</a>
+            <a className="link dim gray f6 f5-ns dib mr3" href="#" title="Store">Decoder</a>
+            <a className="link dim gray f6 f5-ns dib mr3" href="#" title="About">About</a>
+          </div>
+        </nav>
+      </div>
+    </div>
   )
 };
 
